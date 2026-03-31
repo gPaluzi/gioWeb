9010521cca4259ee8e2309300a45f9d6e03258f7
Gio <giovannipaluzi@gmail.com>
2026-03-31 21:36:30 +0700

    modify skills query and templates

--- app/app.py
+++ app/app.py
@@ -1,13 +1,9 @@
 import json
 from flask import Flask, render_template
 from sqlalchemy import select
-from models import Experience
+from models import Experience, Skill
 from database import db_session
 from collections import defaultdict
-
-def load_about_data():
-    with open("./app/data/about.json") as f:
-        return json.load(f)
 
 app = Flask(__name__)
 
@@ -17,16 +13,21 @@ def index():
 
 @app.route("/about")
 def about():
-    query = select(Experience).order_by(Experience.start_date.desc())
-    result = db_session.execute(query)
+    exp_query = select(Experience).order_by(Experience.start_date.desc())
+    exp_result = db_session.execute(exp_query)
 
-    data = defaultdict(list)
-    for exp in result.scalars():
-        data[exp.category].append(exp)
+    exp_data = defaultdict(list)
+    for exp in exp_result.scalars():
+        exp_data[exp.category].append(exp)
 
-    about = load_about_data()
-    
-    return render_template("about.html", about = about, data = data)
+    ski_query = select(Skill).join(Skill.category)
+    ski_result = db_session.execute(ski_query)
+
+    ski_data = defaultdict(list)
+    for ski in ski_result.scalars():
+        ski_data[ski.category.name].append(ski)
+
+    return render_template("about.html", exp_data = exp_data, ski_data = ski_data)
 
 @app.route("/portfolio")
 def portfolio():
